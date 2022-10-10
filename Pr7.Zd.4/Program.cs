a9190ba9ce49e0fdc4d842f4e71066d88b0cc754
USER <USER@DESKTOP-TRQG01D>
2022-10-10 21:07:45 +0300

    Добавьте файлы проекта.

---
+++ Pr7.Zd.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x , y;
+            x = 2.7;
+            y = Math.Log(x + Math.Sqrt(Math.Pow(x, 2) + 9)) - ((x + 1) / (Math.Atan(Math.Pow(x, 3))));
+            Console.WriteLine("y=: {0}", Math.Round(y, 4));
+            Console.ReadLine();
+        }
+    }
+}
